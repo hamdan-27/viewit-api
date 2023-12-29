db1a718f41b622595aafa78991bf616d4551bb7c
hamdan-27 <hamdan102703@gmail.com>
2023-12-29 17:35:02 +0400

    copied the flask starter from cyclic

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def hello():
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico',mimetype='image/vnd.microsoft.icon')
+    return app.send_static_file('favicon.ico')
 
 
 @app.route("/chat/<message>")
--- server.py
+++ server.py
@@ -1,5 +1,11 @@
-from waitress import serve
-from main import app  # Assuming your Flask app instance is named 'app' and is in the 'app' module
+import sys
+from gunicorn.app.wsgiapp import run
+if __name__ == '__main__':
+    sys.argv = "gunicorn --bind 0.0.0.0:5151 app:app".split()
+    sys.exit(run())
 
-if __name__ == "__main__":
-    serve(app)
+# from waitress import serve
+# from main import app  # Assuming your Flask app instance is named 'app' and is in the 'app' module
+
+# if __name__ == "__main__":
+#     serve(app)
