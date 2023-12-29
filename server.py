9c556487455ae768d8f230cfeb3b02db81f47bc6
hamdan-27 <hamdan102703@gmail.com>
2023-12-29 16:32:03 +0400

    minor changes

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ from waitress import serve
 from main import app  # Assuming your Flask app instance is named 'app' and is in the 'app' module
 
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=3000)
+    serve(app, listen='*:3000')
