7e3f6ba9bf5979e316af9e59b5c2c831b52f4dc7
hamdan-27 <hamdan102703@gmail.com>
2023-12-29 16:42:30 +0400

    serve on default

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ from waitress import serve
 from main import app  # Assuming your Flask app instance is named 'app' and is in the 'app' module
 
 if __name__ == "__main__":
-    serve(app, listen='*:3000')
+    serve(app)
