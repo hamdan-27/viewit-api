d36234e51ac610d18ce08e7b54d6f5830151b8dc
hamdan-27 <hamdan102703@gmail.com>
2023-12-29 16:27:24 +0400

    changed port to 3000

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ from waitress import serve
 from main import app  # Assuming your Flask app instance is named 'app' and is in the 'app' module
 
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=8000)
+    serve(app, host='0.0.0.0', port=3000)
