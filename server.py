fb62e59f7ffdb424480ec53dd04b61ec341b09c2
hamdan-27 <hamdan102703@gmail.com>
2023-12-29 17:40:29 +0400

    fixed module name

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import sys
 from gunicorn.app.wsgiapp import run
 if __name__ == '__main__':
-    sys.argv = "gunicorn --bind 0.0.0.0:5151 app:app".split()
+    sys.argv = "gunicorn --bind 0.0.0.0:5151 main:app".split()
     sys.exit(run())
 
 # from waitress import serve
