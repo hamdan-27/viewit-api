4e067c3518cad365e9026cc4be6a61202d30ddcc
hamdan-27 <hamdan102703@gmail.com>
2024-01-03 15:36:23 +0400

    added exception handling and cost information

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify, send_from_directory
+from langchain.callbacks import get_openai_callback
+from flask import Flask, request, jsonify
+from openai import OpenAI
 import dfagent
 import prompts
 import os
@@ -10,7 +12,8 @@ app = Flask(__name__)
 def hello():
     return jsonify({
         "message": "Welcome to the Viewit API! Please navigate to the /chat endpoint" \
-           " followed by your input to use the API."}), 200
+           " for the chatbot API, or the /generate endpoint for the property" \
+            " description API."}), 200
 
 
 @app.route('/favicon.ico')
@@ -21,33 +24,42 @@ def favicon():
 @app.route("/chat/<message>")
 def send_message(message):
 
-    temperature = request.args.get('temperature')
-    model = request.args.get('model')
+    try:
+        temperature = request.args.get('temperature')
+        model = request.args.get('model')
 
-    # AGENT CREATION HAPPENS HERE
-    agent = dfagent.create_pandas_dataframe_agent(
-        model=model or dfagent.model,
-        temperature=temperature or dfagent.TEMPERATURE,
-        df=dfagent.df,
-        prefix=prompts.REIDIN_PREFIX,
-        suffix=prompts.SUFFIX,
-        format_instructions=prompts.FORMAT_INSTRUCTIONS,
-        verbose=True,
-        handle_parsing_errors=True,
-        # max_execution_time=30,
-    )
+        # AGENT CREATION HAPPENS HERE
+        agent = dfagent.create_pandas_dataframe_agent(
+            model=model or dfagent.model,
+            temperature=temperature or dfagent.TEMPERATURE,
+            df=dfagent.df,
+            prefix=prompts.REIDIN_PREFIX,
+            suffix=prompts.SUFFIX,
+            format_instructions=prompts.FORMAT_INSTRUCTIONS,
+            verbose=True,
+            handle_parsing_errors=True,
+            # max_execution_time=30,
+        )
+        
+        with get_openai_callback() as cb:
+            response = agent.run(message)
+            print(cb)
 
-    response = agent.run(message)
-    response_data = {
-        'messages': {
-            "role": "assistant",
-            "content": response
-        },
-        'model': model or dfagent.model,
-        'temperature': (float(temperature) if temperature else "") or dfagent.TEMPERATURE
-    }
+        response_data = {
+            'messages': {
+                "role": "assistant",
+                "content": response
+            },
+            'model': model or dfagent.model,
+            'temperature': (float(temperature) if temperature else "") or dfagent.TEMPERATURE,
+            'total_tokens': str(cb.total_tokens),
+            'total_cost_usd': str(cb.total_cost)
+        }
 
-    return jsonify(response_data), 200
+        return jsonify(response_data), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 
 if __name__ == "__main__":
