4424221e771aa6ee8626c43207c63c8fa6590298
hamdan-27 <hamdan102703@gmail.com>
2024-01-13 17:37:44 +0400

    remove sudo

--- server.py
+++ server.py
@@ -6,6 +6,6 @@ from gunicorn.app.wsgiapp import run
 app = application
 
 if __name__ == "__main__":
-    sys.argv = "sudo gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5151".split()
+    sys.argv = "gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5151".split()
     sys.exit(run())
     # uvicorn.run(app, host="0.0.0.0", port=8181)
