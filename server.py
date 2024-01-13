913244cb7f8be232309f7f6c032fdfc640d911e1
hamdan-27 <hamdan102703@gmail.com>
2024-01-13 17:19:03 +0400

    port 81

--- server.py
+++ server.py
@@ -5,6 +5,6 @@ from gunicorn.app.wsgiapp import run
 
 
 if __name__ == "__main__":
-    sys.argv = "sudo gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80".split()
+    sys.argv = "sudo gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:81".split()
     sys.exit(run())
     # uvicorn.run(app, host="0.0.0.0", port=8181)
