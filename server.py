384e0d74e7e5fade8c5e3d050f34fc78761714d3
hamdan-27 <hamdan102703@gmail.com>
2024-01-13 17:46:34 +0400

    hypercorn

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
 # import uvicorn
-from main import app as application
-import sys
-from gunicorn.app.wsgiapp import run
+# from main import app as application
+# import sys
+# from gunicorn.app.wsgiapp import run
 
-app = application
 
-if __name__ == "__main__":
-    sys.argv = "gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5151".split()
-    sys.exit(run())
+# if __name__ == "__main__":
+#     sys.argv = "gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5151".split()
+#     sys.exit(run())
+
     # uvicorn.run(app, host="0.0.0.0", port=8181)
+
+from hypercorn import run
+from main import app
+
+if __name__ == '__main__':
+    run(app, bind="0.0.0.0:8000")
